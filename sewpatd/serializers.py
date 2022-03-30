80156645dc8912947a95c9203e3bc14d0bdfcbeb
Jean-Pierre Merx <jean-pierre.merx@aictivate.com>
2022-03-30 14:42:28 +0200

    Catch non-unique sewing pattern / drawing name error

--- sewpatd/serializers.py
+++ sewpatd/serializers.py
@@ -1,6 +1,9 @@
 #  Copyright (c) 2020. Jean-Pierre Merx All rights reserved.
 
+
+from django.db import IntegrityError
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 from .models import SewingPattern, Drawing
 
@@ -20,5 +23,8 @@ class DrawingSerializer(serializers.ModelSerializer):
     def create(self, data):
         sewing_pattern = SewingPattern.objects.get(pk=self.context["view"].kwargs["pattern_pk"])
         data['sewing_pattern'] = sewing_pattern
-        obj = super().create(data)
+        try:
+            obj = super().create(data)
+        except IntegrityError as e:
+            raise ValidationError(repr(e))
         return obj
